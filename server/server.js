ee9f045a9a524236744f1de312104aa955c91fb9
Jeffry <khojeff89gmail.com>
2025-05-23 02:12:40 +0700

    create endpoint user

--- server/server.js
+++ server/server.js
@@ -3,6 +3,13 @@ const routes = require('./server-routes.js');
 
 const port = process.env.PORT || 5000;
 
+// USER
+app.get('/user', routes.getAllUsers);
+app.get('/user/:id', routes.getUser);
+
+app.post('/user', routes.postUser);
+
+// TO DO
 app.get('/', routes.getAllTodos);
 app.get('/:id', routes.getTodo);
 
@@ -11,6 +18,7 @@ app.patch('/:id', routes.patchTodo);
 
 app.delete('/', routes.deleteAllTodos);
 app.delete('/:id', routes.deleteTodo);
+
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => console.log(`Listening on port ${port}`));
